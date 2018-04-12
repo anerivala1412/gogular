56824ad8a21bed0168714f714b0e7113c0239402
Aneri <jitendra@stimulusconsultancy.com>
2018-04-12 23:36:31 +0530

    step1 adding source to destination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA , ApplicationRef  } from '@angular/core';
 import { AppComponent } from './app.component';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
+import { FormsModule , ReactiveFormsModule} from '@angular/forms';
+import { AgmCoreModule } from '@agm/core';
+import { DirectionsMapDirective } from './google-map.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DirectionsMapDirective
   ],
   imports: [
-    BrowserModule
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAGxBc7Ei2xIYbeOCCN0C8maGVB3zfMWpM',
+      libraries: ["places"]
+    }),
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule.forRoot()
   ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
